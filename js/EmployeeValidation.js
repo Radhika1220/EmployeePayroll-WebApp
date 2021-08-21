b9d2297760e456bc056465a2b0a5eaef5d2fc3a0
Radhika S <radhika.s@tvsnext.io>
2021-08-21 18:53:04 +0530

    [radhika] Add: storing the data in local storage

--- js/EmployeeValidation.js
+++ js/EmployeeValidation.js
@@ -49,13 +49,27 @@ const save=()=>
   try
   {
     let employeePayrollData=createEmployeePayroll();
+    createAndUpdateStorage(employeePayrollData);
   }
   catch(e)
   {
     return;
   }
 }
-
+//storing in local storage
+function createAndUpdateStorage(employeePayrollData)
+{
+  let employeePayrollList=JSON.parse(localStorage.getItem("EmployeePayrollList"));
+  if(employeePayrollList!=undefined)
+  {
+    employeePayrollList.push(employeePayrollData);
+  }
+  else{
+    employeePayrollList=[employeePayrollData];
+  }
+  alert(employeePayrollList.toString());
+  localStorage.setItem("EmployeePayrollList",JSON.stringify(employeePayrollList));
+}
 
 const createEmployeePayroll=() =>
 {
